1ab099b61f189f3a8b139c352719724dfddc81cd
Hodan-1 <hodanabdi24@outlook.com>
2025-03-17 15:05:18 +0000

    Fixed logic and added a retry logic

--- docker/docker_compose/messenger.py
+++ docker/docker_compose/messenger.py
@@ -1,31 +1,42 @@
 import pika
 import numpy as np
 import matplotlib.pyplot as plt
+import json
 
-# List of file paths to process
+##Added the needed files
 file_paths = [
     "https://atlas-opendata.web.cern.ch/atlas-opendata/samples/2020/4lep/Data/data_A.4lep.root",
     "https://atlas-opendata.web.cern.ch/atlas-opendata/samples/2020/4lep/Data/data_B.4lep.root",
-    # Add more file paths as needed
+    "https://atlas-opendata.web.cern.ch/atlas-opendata/samples/2020/4lep/Data/data_C.4lep.root",
+    "https://atlas-opendata.web.cern.ch/atlas-opendata/samples/2020/4lep/Data/data_D.4lep.root",
+    
 ]
 
-# Connect to RabbitMQ
-connection = pika.BlockingConnection(pika.ConnectionParameters('rabbitmq'))
+
+# Connect to RabbitMQ with retry logic
+def connect_to_rabbitmq():
+    while True:
+        try:
+            connection = pika.BlockingConnection(pika.ConnectionParameters('rabbitmq'))
+            return connection
+        except pika.exceptions.AMQPConnectionError:
+            print("Waiting for RabbitMQ...")
+            time.sleep(5)
+
+connection = connect_to_rabbitmq()
 channel = connection.channel()
 
-# Declare queues
 channel.queue_declare(queue='task_queue', durable=True)
 channel.queue_declare(queue='result_queue', durable=True)
 
-# Send tasks to RabbitMQ
-for file_path in file_paths:
-    channel.basic_publish(exchange='',
-                          routing_key='task_queue',
-                          body=file_path)
 
-print(" [x] Sent tasks to workers")
+# Send tasks to the queue
+for file in file_paths:
+    task = {'file_path': file}
+    channel.basic_publish(exchange='', routing_key='task_queue', body= file)
+    print(f"Sent task: {task}")
 
-# Collect results from workers
+# Collect results from workers. combined agregator to this...
 results = []
 def collect_results(ch, method, properties, body):
     results.extend(eval(body.decode()))
@@ -41,5 +52,6 @@ def collect_results(ch, method, properties, body):
 
 channel.basic_consume(queue='result_queue', on_message_callback=collect_results)
 
-print(' [*] Waiting for results. To exit press CTRL+C')
-channel.start_consuming() 
+print(' Waiting for results.')
+channel.start_consuming()
+
--- docker/docker_compose/worker.py
+++ docker/docker_compose/worker.py
@@ -15,10 +15,10 @@ GeV = 1.0
 # Define mass  functions
 def cut_lep_type(lep_type):
     sum_lep_type = lep_type[:, 0] + lep_type[:, 1] + lep_type[:, 2] + lep_type[:, 3]
-    return (sum_lep_type != 44) & (sum_lep_type != 48) & (sum_lep_type != 52)
+    return (sum_lep_type == 44) & (sum_lep_type == 48) & (sum_lep_type == 52)
 
 def cut_lep_charge(lep_charge):
-    return lep_charge[:, 0] + lep_charge[:, 1] + lep_charge[:, 2] + lep_charge[:, 3] != 0
+    return lep_charge[:, 0] + lep_charge[:, 1] + lep_charge[:, 2] + lep_charge[:, 3] == 0
 
 def calc_mass(lep_pt, lep_eta, lep_phi, lep_E):
     p4 = vector.zip({"pt": lep_pt, "eta": lep_eta, "phi": lep_phi, "E": lep_E})
@@ -32,12 +32,11 @@ def process_task(file_path):
     data = tree.arrays(variables, library="ak")
 
 # Apply cuts
-    data = data[~cut_lep_type(data['lep_type'])]
-    data = data[~cut_lep_charge(data['lep_charge'])]
+    data = data[cut_lep_type(data['lep_type'])]
+    data = data[cut_lep_charge(data['lep_charge'])]
     
     # Calculate invariant mass
     data['mass'] = calc_mass(data['lep_pt'], data['lep_eta'], data['lep_phi'], data['lep_E'])
-    
     return data
 
 def callback(ch, method, properties, body):
@@ -52,8 +51,19 @@ def callback(ch, method, properties, body):
     channel.basic_publish(exchange='', routing_key='result_queue', body=str(ak.to_list(processed_data['mass'])))
     ch.basic_ack(delivery_tag=method.delivery_tag)
 
-# Connect to RabbitMQ
-connection = pika.BlockingConnection(pika.ConnectionParameters('rabbitmq'))
+def connect_to_rabbitmq():
+## failing to connect so added a logic that retries:
+    while True:
+        try:
+            # Connect to RabbitMQ
+            connection = pika.BlockingConnection(pika.ConnectionParameters('rabbitmq'))
+            return connection
+    
+        except pika.exceptions.AMQPConnectionError:
+            print("Waiting for RabbitMQ...")
+            time.sleep(5)
+
+connection = connect_to_rabbitmq()
 channel = connection.channel()
 
 # Declare queues
