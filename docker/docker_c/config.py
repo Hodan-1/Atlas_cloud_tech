1990ba69f186af4b28c8c4b47b0bfd9fede5deb0
Hodan-1 <hodanabdi24@outlook.com>
2025-03-20 01:01:44 +0000

    adding test files

--- docker/docker_c/config.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-MeV = 0.001
-GeV = 1.0
-
-RABBITMQ_HOST = os.getenv("RABBITMQ_HOST", "rabbitmq")
-BASE_PATH = os.getenv("BASE_PATH", "https://atlas-opendata.web.cern.ch/atlas-opendata/samples/2020/4lep/")
